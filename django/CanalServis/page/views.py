3d1f4227b6c4929b87b76edac7529ea7a6e6168b
Dmitriy Nikonov <NowReborngg@gmail.com>
2022-07-29 01:14:20 +0300

    гз

--- django/CanalServis/page/views.py
+++ django/CanalServis/page/views.py
@@ -4,7 +4,8 @@ from django.template import loader
 from .models import *
 # Create your views here.
 def index(request):
-
+    """Index View
+    """
     data = Order.objects.all()
     total = sum([d.dollar for d in data])
     template = loader.get_template('pages/index.html')
